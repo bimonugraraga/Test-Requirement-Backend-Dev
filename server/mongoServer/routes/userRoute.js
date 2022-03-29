0e56d874674edb4b48a4505b3defddda883017d4
Erno <bimonugraraga@gmail.com>
2022-03-29 14:57:26 +0700

    perisapan deploy mongo

--- server/mongoServer/routes/userRoute.js
+++ server/mongoServer/routes/userRoute.js
@@ -3,9 +3,7 @@ const router = express.Router()
 const UserController = require('../controllers/userControllers')
 
 
-router.get('/',  (req, res) => {
-  res.status(200).json("IN USER ROUTE")
-})
+
 
 //!Register User
 router.post('/register', UserController.registerNewUser)
